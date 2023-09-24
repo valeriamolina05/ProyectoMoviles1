a1f9c1d28c7d136d74fe5024a16d667bb39ab764
ValeriaM <valemolinac05@gmail.com>
2023-09-24 18:11:47 -0500

    Se inicia proyecto

---
+++ TuristiandoAPP2Comfama/src/main/java/org/example/Turismo.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.entidades.*;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Turismo {
+    public static void main(String[] args) {
+
+        //Usuario usuario = new {Usuario();
+        Afiliado afiliado = new Afiliado();
+        Scanner lea = new Scanner(System.in);
+
+        //usuario.setNombres("Valeria Molina");
+
+        afiliado.setNombres("Karol");
+        afiliado.setValorMensualidad(-10000);
+    }
+}
---
+++ TuristiandoAPP2Comfama/src/test/java/org/example/validaciones/OcasionalValidacionTest.java
@@ -0,0 +1,33 @@
+package org.example.validaciones;
+
+import org.example.utilidades.Mensaje;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OcasionalValidacionTest {
+
+    private OcasionalValidacion ocasionalValidacion;
+
+    @BeforeEach
+    public void configurarPruebas(){
+        this.ocasionalValidacion = new OcasionalValidacion();
+    }
+
+    @Test
+    public void validarCostoCuotaCorrecto(){
+        Integer costoPrueba = 10000;
+        Boolean respuesta = Assertions.assertDoesNotThrow(() ->this.ocasionalValidacion.validarValorCuota(costoPrueba));
+        Assertions.assertTrue(respuesta);
+    }
+
+    @Test
+    public void validarCostoCuotaIncorrecto(){
+        Integer costoPrueba = -10000;
+        Exception respuesta = Assertions.assertThrows(Exception.class,() ->this.ocasionalValidacion.validarValorCuota(costoPrueba));
+        Assertions.assertEquals(Mensaje.COSTO_PERSONA_INVALIDO.getMensaje(), respuesta.getMessage());
+    }
+
+}
